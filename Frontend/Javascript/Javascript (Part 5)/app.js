969c9673effeb2ab639b2f7177b21d2ce35c5cc6
himanshudash132 <Himanshudash10@gmail.com>
2025-09-08 16:52:57 +0530

    new

---
+++ Frontend/Javascript/Javascript (Part 5)/app.js
@@ -0,0 +1,180 @@
+// objefct literals
+// creating object literals
+
+// let delhi = {
+//     latitude: "28.7041 N",
+//     longitude: "77.1025 E"
+// };
+
+// const student = {
+//     name: "shradha",
+//     age: 23,
+//     marks: 94.4,
+//     city: "Delhi"
+// };
+// console.log(delhi);
+// console.log(student);
+
+
+
+// creating a post
+
+// const post = {
+//     username: "himanshu4official",
+//     content: "this is cool tech",
+//     likes: 900,
+//     reposts: 5,
+//     tags: ["mit","harvard","yale","caltech","stanford"]
+// };
+
+// console.log(post);
+
+
+
+// get values
+
+// const post = {
+//     username: "himanshu4official",
+//     content: "this is cool tech",
+//     likes: 900,
+//     reposts: 5,
+//     tags: ["mit","harvard","yale","caltech","stanford"]
+// };
+
+// // console.log(post);
+// console.log(post["username"]);
+// console.log(post["content"]);
+// console.log(post["likes"]);
+// console.log(post["reposts"]);
+// console.log(post["tags"]);
+// console.log("---------------------");
+// console.log("---------------------");
+// console.log(post.username);
+// console.log(post.content);
+// console.log(post.likes);
+// console.log(post.reposts);
+// console.log(post.tags);
+// console.log(post.tags[2]);
+
+
+
+// conversion in get values
+
+// const obj = {
+//     1: "a",
+//     2: "b",
+//     null:"d",
+//     true:"c",
+//     undefined: "e"
+// };
+// console.log(obj);
+
+
+// add/ update values
+
+// const student = {
+//     name: "shradha",
+//     age: 23,
+//     marks: 94.4,
+//     city: "Delhi"
+// };
+
+// console.log(student);
+// console.log(student.city = "mumbai");
+// console.log(student);
+// console.log(student.gender = "female");
+// console.log(student);
+// console.log(student.marks = "A");
+// console.log(student);
+// console.log(student.marks = [99,88,77,66]);
+// console.log(student);
+// console.log(delete student.marks);
+// console.log(student);
+
+
+
+
+// nested objestd
+
+// const classInfo = {
+//     aman : {
+//         grade: "A+",
+//         city: "Delhi"
+//     },
+//     shradha : {
+//         grade: "A",
+//         city: "Pune"
+//     },
+//     karan : {
+//         grade: "A+",
+//         city: "Mumbai"
+//     }
+// };
+
+// console.log(classInfo.aman);
+// console.log(classInfo.aman.grade);
+// console.log(classInfo.shradha);
+// console.log(classInfo.shradha.city);
+// console.log(classInfo.karan);
+// console.log(classInfo.karan.city = "patna");
+
+
+
+
+
+// array of objects
+
+// const classInfo = [
+//      {
+//         name: "aman",
+//         grade: "A+",
+//         city: "Delhi"
+//     },
+//     {
+//         name: "shradha",
+//         grade: "A",
+//         city: "Pune"
+//     },
+//     {
+//         name: "karan",
+//         grade: "O",
+//         city: "Mumbai"
+//     }
+// ];
+
+// console.log(classInfo[0]);
+// console.log(classInfo[0].name);
+// console.log(classInfo[0].grade);
+// console.log(classInfo[0].city);
+// console.log(classInfo[0].city = "Patna");
+// console.log(classInfo[0].gender = "female");
+// console.log(classInfo[0]);
+
+
+
+// math object
+
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.abs(-122));
+// console.log(Math.pow(2, 4));
+// console.log(Math.floor(1.234567));
+// console.log(Math.floor(5.5));
+// console.log(Math.ceil(1.234567));
+// console.log(Math.ceil(5.5));
+// console.log(Math.random());
+
+
+
+// Random integers
+
+
+
+
+// guessing game
+
+
+
+
+
+
