cd7ad465dd4d205ba3e19ae3746df35fd01afee5
himanshudash132 <Himanshudash10@gmail.com>
2025-09-08 08:32:39 +0530

    new

---
+++ Frontend/Javascript/Javascript (Part 4)/asg.js
@@ -0,0 +1,55 @@
+// let arr = [1,2,3,4,5,6,2,3];
+// let num = 2;
+
+// for(let i = 0; i < arr.length; i++) {
+//     if (arr[i] == num) {
+//         arr.splice(i,1);
+//     }
+// }
+// console.log(arr)
+
+
+
+// let number = 27152;
+// let count = 0;
+// let copy = number;
+// while (copy > 0) {
+//     count++;
+//     copy = Math.floor(copy/10);
+// }
+// console.log(count);
+
+
+
+
+// let number = 27152;
+// let sum = 0;
+// let copy = number;
+// while (copy > 0) {
+//     digit = copy % 10;
+//     sum += digit;
+//     copy = Math.floor(copy/10);
+// }
+// console.log(sum);
+
+
+
+// let number = 5;
+// let factorial = 1;
+
+// for (let i = 1; i <= number; i++) {
+//     factorial *= i;
+// }
+// console.log(factorial);
+
+
+
+// let arr=[2,5,10,4,2,7,1,9];
+// let largest = 0;
+
+// for (let i = 0; i<= arr.length; i++) {
+//     if (arr[i] > largest) {
+//         largest = arr[i];
+//     }
+// }
+// console.log(largest);
